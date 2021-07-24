8a8ec47b480f312c93cb7c9ee2fd7df0508d999a
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-24 17:52:17 +0000

    adds databse table schema

---
+++ server/db/schema/03_comments.sql
@@ -0,0 +1,8 @@
+-- drop and create comments table
+DROP TABLE IF EXISTS comments CASCADE;
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  content TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ server/db/schema/07_categories_resources.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS categories_resources CASCADE;
+CREATE TABLE categories_resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
+  resource_id INTEGER NOT NULL REFERENCES resources(id) ON DELETE CASCADE,
+);
