19a4af36020e8f8d7ae38f4c55dbac2f4cc00a0b
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-24 19:35:56 +0000

    adds database seed data

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,3 +1,11 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- data from https://www.mockaroo.com/
+-- 6 users + 1 test
+
+insert into users (name, email) values ('test', 'test@gmail.com');
+insert into users (name, email) values ('Pam', 'pcayford0@unicef.org');
+insert into users (name, email) values ('Hernando', 'hpatroni1@cargocollective.com');
+insert into users (name, email) values ('Dare', 'dmarzello2@e-recht24.de');
+insert into users (name, email) values ('Kassey', 'kholstein3@last.fm');
+insert into users (name, email) values ('Maisie', 'mwhitwam4@psu.edu');
+insert into users (name, email) values ('Rollin', 'rkuhnhardt5@pcworld.com');
+
---
+++ server/db/seeds/06_categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (user_id, name)
+VALUES (1, 'Space'),
+        (1, 'Cooking'),
+        (1, 'Sports');
