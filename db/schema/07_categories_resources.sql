d944422be08c343e4255bab8a61a7709faeed368
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-24 19:53:58 +0000

    fixes bugs with database setup - database is able to be reset and seeded now

--- db/schema/07_categories_resources.sql
+++ db/schema/07_categories_resources.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS categories_resources CASCADE;
 CREATE TABLE categories_resources (
   id SERIAL PRIMARY KEY NOT NULL,
   category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
-  resource_id INTEGER NOT NULL REFERENCES resources(id) ON DELETE CASCADE,
+  resource_id INTEGER NOT NULL REFERENCES resources(id) ON DELETE CASCADE
 );
