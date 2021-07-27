0726677e0aea8ff1b89c849c21bafd78065b0cab
Alex M <milesAwayAlex@gmail.com>
2021-07-26 17:22:39 -0700

    Add POST to /likes, avg_rating for resources, switch resources search to name, chart the API map

--- routes/resources.js
+++ routes/resources.js
@@ -9,15 +9,41 @@ module.exports = (db) => {
   router.get("/search", (req, res) => {
     const { u } = req.query;
     db.query(
-      "SELECT * FROM resources JOIN users ON users.id = user_id WHERE user_id = $1",
-      [u]
+      `
+SELECT resources.id AS res_id,
+  resources.user_id AS auth_id,
+  url,
+  title,
+  description,
+  resources.created_at AS res_timestamp,
+  round(avg(ratings.value), 2) AS avg_rating
+FROM resources
+  JOIN ratings ON resource_id = resources.id
+  JOIN users ON users.id = resources.user_id
+WHERE users.name LIKE $1::varchar
+GROUP BY resources.id`,
+      [`%${u}%`],
     )
       .then((data) => res.json(data.rows))
       .catch((e) => res.status(500).json({ error: e.message }));
   });
   router.get("/:id", (req, res) => {
     const { id } = req.params;
-    db.query("SELECT * FROM resources WHERE resources.id = $1", [id])
+    db.query(
+      `
+SELECT resources.id AS res_id,
+  resources.user_id AS auth_id,
+  url,
+  title,
+  description,
+  resources.created_at AS res_timestamp,
+  round(avg(ratings.value), 2) AS avg_rating
+FROM resources
+  JOIN ratings ON resource_id = resources.id
+WHERE resources.id = $1
+GROUP BY resources.id`,
+      [id],
+    )
       .then((data) => res.json(data.rows))
       .catch((e) => res.status(500).json({ error: e.message }));
   });
@@ -26,7 +52,7 @@ module.exports = (db) => {
     const { url, title, description } = req.body;
     db.query(
       "INSERT INTO resources (user_id, url, title, description) VALUES ($1, $2, $3, $4) RETURNING *",
-      [user_id, url, title, description]
+      [user_id, url, title, description],
     )
       .then((data) => {
         res.json(data.rows);
