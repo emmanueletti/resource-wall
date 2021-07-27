df40910644136d16750a26cb8d1806329dc0d891
Alex M <milesAwayAlex@gmail.com>
2021-07-27 15:23:11 -0700

    Add unique statements to likes and ratings

--- db/schema/05_ratings.sql
+++ db/schema/05_ratings.sql
@@ -3,5 +3,6 @@ CREATE TABLE ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   resource_id INTEGER NOT NULL REFERENCES resources(id) ON DELETE CASCADE,
-  value INT NOT NULL
+  value INT NOT NULL,
+  UNIQUE (user_id, resource_id)
 );
